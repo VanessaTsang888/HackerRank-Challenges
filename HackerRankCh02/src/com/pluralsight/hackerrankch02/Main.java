b893a5e9e68faf537ae3576950bd063c5806810b
Vanessa Tsang <info@vanessatsang.uk>
2019-09-24 13:35:57 +0100

    add HackerRankCh02

---
+++ HackerRankCh02/src/com/pluralsight/hackerrankch02/Main.java
@@ -0,0 +1,19 @@
+package com.pluralsight.hackerrankch02;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// print x3 ints on separate lines.
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    }
+}
