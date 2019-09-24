8b644d15149245d3edafe48d6571978bedd448c2
Vanessa Tsang <info@vanessatsang.uk>
2019-09-24 13:30:13 +0100

    Add solution to Java challenge 1

---
+++ HackerRankCh01/src/com/pluralsight/hackerrankch01/Main.java
@@ -0,0 +1,12 @@
+package com.pluralsight.hackerrankch01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// print Hello, World.
+        // print Hello, Java.
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
